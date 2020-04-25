973f3d1e53a31c280a6ed038dc0c0175f3b25840
Uzytkownik <uzytkownik@kursgit.pl>
2020-04-25 10:29:33 +0200

    initial UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner SCANNER;
     public static void main(String[] args) {
+        SCANNER = new Scanner(System.in);
         System.out.println("Robimy biblioteke");
+
+        System.out.println("Witaj w bibliotece! Co mogę dla Ciebie zrobić??");
+
+        while(true) {
+            menu();
+            int choose = SCANNER.nextInt();
+            switch (choose) {
+                case 1:
+                    System.out.println("Not implemented yet");
+                    break;
+                case 2:
+                    System.out.println("Not implemented yet");
+                    break;
+                case 3:
+                    System.out.println("Not implemented yet");
+                    break;
+                default:
+                    System.out.println("Zła komenda, podaj liczbę jeszcze raz");
+            }
+        }
+    }
+
+    private static void menu() {
+        System.out.println("1) Wyświetl wszystkie książki");
+        System.out.println("2) Dodaj książkę");
+        System.out.println("3) Usuń książkę");
+        System.out.println("Wybieram: ");
     }
 }
